2f089183abf022b5a0b0f2109cb64f20ebfd4e20
Krystian1999 <krystian.g1999@wp.pl>
2020-09-03 23:35:30 +0200

    get your salary & give yourself a rise

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,11 +1,37 @@
 package com.company;
 
+
+import java.util.Date;
+
 public class Human {
     public Animal pet;
     public Car car;
+    private Double salary = 1000.0;
+
+    Date date = new Date();
 
 
     public void setCar(Car car){
         this.car = car;
+    }
+
+    public Double getSalary() {
+        System.out.println(date + " " + salary);
+        return salary;
+    }
+
+    public Double setSalary(Double salary) {
+
+        if (salary < 0) {
+            System.out.println("wartosc musi byc dodatnia");
+        } else {
+            System.out.println("Nowe dane zostały wysłane do systemu ksiegowania ");
+            System.out.println("Musisz odebrac aneks do umowy od Hani");
+            System.out.println("Zus i US wiedzą o zmianie wypłaty");
+            System.out.println("Twoje nowe wynagrodzenie wynosi:" + salary*1.1);
+            return this.salary = salary*1.1;
+
+        }
+        return this.salary = salary;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,6 +19,7 @@ public class Main {
 
         me.setCar(bmw);
 
+        me.getSalary();
     }
 }
 
